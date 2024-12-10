17b72293805f18c137028eb12fafe2f453bf3377
jalyice <jalyice@gmail.com>
2024-12-10 15:01:30 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/bookstoreeureka/BookStoreEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoreeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/bookstoreeureka/BookStoreEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoreeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
